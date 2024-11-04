33826f62aa993ce89b735fe4cb565bf2cfc965bc
Marcela Stallone <chela@MacBook-Air-de-Marcela.local>
2024-11-04 14:11:58 -0400

    primer commit

---
+++ src/proy2edd/Proy2EDD.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proy2edd;
+import proy2edd.interfaces.InterfazUnica;
+/**
+ *
+ * @author chela
+ */
+public class Proy2EDD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        InterfazUnica proy2 = new InterfazUnica();
+        proy2.show();
+    }
+    
+}
